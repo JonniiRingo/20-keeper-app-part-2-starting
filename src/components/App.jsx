993003fec25972e297bd9d8da51fe574d88e4aff
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-12-04 22:25:45 -0800

    interim save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"; 
+
+console.log(notes); 
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+
+      <Note 
+        title= "This is the note title"
+        content= "This is the note content"
+      />
+
       <Footer />
     </div>
   );
