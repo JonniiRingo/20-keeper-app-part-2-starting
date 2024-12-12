7e30251016185f7799c37df642569a7af6cfe17e
Jonnii Ringo <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-12-11 16:33:50 -0800

    Intermediate save

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,8 +5,8 @@ import React from "react";
 function Note(props) {
   return (
     <div className="note">
-      <h1>props.title</h1>
-      <p>props.content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
 
     </div>
   );
